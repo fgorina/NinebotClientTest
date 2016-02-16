94eb85e901fdd63dc548f408caeaf79db1714007
Paco Gorina <fgorina@gmail.com>
2016-02-16 20:10:31 +0100

    Very simplified. Esentially handles a list of log files that may be shared thanks to the accessory button or each line. A click in the line loads the file to the Dashboard/graphics A click in the accessory info button opens the share view wich includes access to iCloud Drive New allows to enter normal client mode Edit allows deleting files Debug launches the MiM

--- NinebotClientTest/ViewController.swift
+++ NinebotClientTest/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource, UITableViewDelegate{
     
     enum stateValues {
         case Waiting
@@ -19,27 +19,15 @@ class ViewController: UIViewController {
     }
     
 
-    
+    var ninebot : BLENinebot = BLENinebot()
     var server : BLESimulatedServer?
     var client : BLESimulatedClient?
     
     var state : stateValues = .Waiting
     
-    
-    var buffer = [UInt8]()
-    
-    var datos = BLENinebot()
-    
     var firstField = 185      // Primer camp a llegir
     var nFields = 10         // Numero de camps a llegir
     var timerStep = 0.01   // Segons per repetir el enviar
-    var contadorOp = 0    // Indica quantes solicituts s'han enviar
-    
-    var listaOp :[(UInt8, UInt8)] = [(34,9), (50,2), (58,5), (71,10), (97,4), (181,10)]
-    
-    var headersOk = false
-    
-    var sendTimer : NSTimer?    // Timer per enviar les dades periodicament
     
     @IBOutlet weak var timerField   : UITextField!
     @IBOutlet weak var valuesField   : UITextField!
@@ -48,9 +36,25 @@ class ViewController: UIViewController {
     
     var dashboard : BLENinebotDashboard?
     
+    var files = [NSURL]()
+    var actualDir : NSURL?
+    var currentFile : NSURL?
+    
+    @IBOutlet weak var tableView : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Lookup files
+        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+        
+        let docsUrl = appDelegate?.applicationDocumentsDirectory()
+        
+        if let docs = docsUrl{
+            
+            loadLocalDirectoryData(docs)
+         }
     }
     
     override func didReceiveMemoryWarning() {
@@ -58,82 +62,142 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func loadLocalDirectoryData(dir : NSURL){
+        
+        self.actualDir = dir
+        
+        files.removeAll()
+        
+        let mgr = NSFileManager()
+        
+        
+        let enumerator = mgr.enumeratorAtURL(dir, includingPropertiesForKeys: nil, options: [NSDirectoryEnumerationOptions.SkipsHiddenFiles, NSDirectoryEnumerationOptions.SkipsSubdirectoryDescendants]) { (url:NSURL, err:NSError) -> Bool in
+                NSLog("Error enumerating files %@", err)
+            return true
+        }
+        
+        if let arch = enumerator{
+        
+            for url in arch {
+                
+                files.append(url as! NSURL)
+                
+            }
+        }
+        
+        
+    }
+    
     @IBAction func MiM(){
-        getScreenData()
-        
-        self.client = BLESimulatedClient()
-        if let cli0 = self.client {
-            cli0.startScanning()
-            cli0.cntrl = self
-        }
-        
-        self.server = BLESimulatedServer()
-        if let srv0 = self.server {
-            srv0.cntrl = self
-        }
         
     }
     
     
     @IBAction func Server(){
-        getScreenData()
-        
-        if state == .Server{
-            
-            if let srv = self.server{
-                srv.stopTransmiting()
-            }
-            
-            server = nil
-            state = .Waiting
-        }
-        else if state == .Waiting {
-            self.server = BLESimulatedServer()
-        }
-    }
+     }
     
     @IBAction func Client(){
-        getScreenData()
-        
-        if state == .Client {
-            if let cli = self.client {
-                cli.stopScanning()
-            }
-            
-            client = nil
-            state = .Waiting
-        }
-        else if state == .Waiting {
-            self.client = BLESimulatedClient()
-            if let cli = self.client {
-                cli.cntrl = self
-                cli.startScanning()
-            }
-        }
-    }
+      }
     
     
     func startClient (){
-        if state == .Waiting {
-            self.client = BLESimulatedClient()
-            if let cli = self.client {
-                cli.cntrl = self
-                cli.startScanning()
-            }
-        }
-    }
+      }
     
     func stopClient(){
-        
-        if state == .Client {
-            if let cli = self.client {
-                cli.stopScanning()
-            }
-            
-            client = nil
-            state = .Waiting
-        }
 
+    }
+    
+    // MARK: UITableViewDataSource
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.files.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("fileCellIdentifier", forIndexPath: indexPath)
+        
+        let url = files[indexPath.row]
+        
+        let name = NSFileManager.defaultManager().displayNameAtPath(url.path!)
+        var rsrc : AnyObject? = nil
+        
+        do{
+            try url.getResourceValue(&rsrc, forKey: NSURLCreationDateKey)
+        }
+        catch{
+            NSLog("Error reading creation date of file %", url)
+        }
+        
+        let date = rsrc as? NSDate
+        
+        cell.textLabel!.text = name
+        
+        if let dat = date {
+        
+            let fmt = NSDateFormatter()
+            fmt.dateStyle = NSDateFormatterStyle.ShortStyle
+            fmt.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+            let s = fmt.stringFromDate(dat)
+            
+            cell.detailTextLabel!.text = s
+        }
+        
+        
+         return cell
+    }
+    
+  
+    // MARK: UITableViewDelegate
+    
+    
+    // Override to support editing the table view.
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+    if editingStyle == .Delete {
+        let url = self.files[indexPath.row]
+        
+        let mgr = NSFileManager.defaultManager()
+        do {
+            try mgr.removeItemAtURL(url)
+            self.files.removeAtIndex(indexPath.row)
+            //tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }catch{
+            NSLog("Error removing %@", url)
+        }
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+    }
+    
+
+    
+    func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath) {
+        
+        self.currentFile = self.files[indexPath.row]
+        self.shareData(self.currentFile, src: tableView)
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        self.currentFile = self.files[indexPath.row]
+        
+        if let file = self.currentFile{
+            self.ninebot.loadTextFile(file)
+        }
+        
+        
+        self.performSegueWithIdentifier("openFileSegue", sender: self)
     }
     // MARK : Screen recovery functions
     
@@ -161,9 +225,19 @@ class ViewController: UIViewController {
         if segue.identifier == "dashboardSegue" {
              if let dash = segue.destinationViewController as? BLENinebotDashboard{
                 dash.delegate = self
+                dash.ninebot = self.ninebot
                 self.dashboard = dash
-                self.startClient() // Tan sols en algun cas potser depenent del sender?
+                dash.connect()
 
+            }
+        }else if segue.identifier == "openFileSegue"{
+            if let dash = segue.destinationViewController as? BLENinebotDashboard{
+                dash.delegate = self
+                dash.ninebot = self.ninebot
+                self.dashboard = dash
+                
+                //self.startClient() // Tan sols en algun cas potser depenent del sender?
+                
             }
         }
     }
@@ -180,35 +254,28 @@ class ViewController: UIViewController {
     }
     
     func clientStarted(){
-        
-        self.datos.clearAll()
-        self.contadorOp = 0
-        self.headersOk = false
-        self.state = .Client
-        
-        self.sendTimer = NSTimer.scheduledTimerWithTimeInterval(self.timerStep, target: self, selector: "sendData:", userInfo: nil, repeats: true)
     }
     
     func clientStopped(){
         
-        if let tim = self.sendTimer{
-            tim.invalidate()
-            self.sendTimer = nil
-        }
+        let aFile = self.ninebot.createTextFile()
+        self.shareData(aFile, src: self.tableView)
 
-        self.client = nil
-        self.state = .Waiting
-        let aFile = self.datos.createTextFile()
-
-
-        self.shareData(aFile)
-        
-        
      }
+    
+    @IBAction func  editFiles(src: AnyObject){
+        if self.tableView.editing{
+            self.tableView.editing = false
+        }
+        else{
+            self.tableView.editing = true
+        }
+        
+    }
     
     // Create a file with actual data and share it
     
-    func shareData(file: NSURL?){
+    func shareData(file: NSURL?, src:AnyObject){
         
         
         if let aFile = file {
@@ -223,6 +290,12 @@ class ViewController: UIViewController {
                 
             }
             
+            activityViewController.popoverPresentationController?.sourceView = src as? UIView
+            
+            activityViewController.modalPresentationStyle = UIModalPresentationStyle.FormSheet
+            
+        
+            
             self.presentViewController(activityViewController,
                 animated: true,
                 completion: nil)
@@ -231,65 +304,7 @@ class ViewController: UIViewController {
         
     }
     
-    // MARK: Client operations
-    
-    func sendData(timer:NSTimer){
-        
-        if self.datos.checkHeaders() {  // Get normal data
-            
-            if !self.headersOk {
-                
-                if let dash = self.dashboard {
-                    
-                    let sn = datos.serialNo()
-                    let v1 = datos.version()
-                    
-                    let title = String(format:"%@ (%d.%d.%d)", sn, v1.0, v1.1, v1.2)
-                    
-                    dash.updateTitle(title)
-                }
-                
-            }
-            
-            let (op, l) = listaOp[contadorOp++]
-            
-            if contadorOp >= listaOp.count{
-                contadorOp = 0
-            }
-            
-            let message = BLENinebotMessage(com: op, dat:[ l * 2] )
-            
-            if let cli = self.client{
-                if let dat = message?.toNSData(){
-                    
-                    cli.writeValue(dat)
-                }
-            }
-            else{
-                timer.invalidate()
-                self.sendTimer = nil
-                self.appendToLog("Error : Intentant enviar missatge amb client = nil")
-            }
-            
-            
-            
-        } else {    // Get One time data
-            
-            let message = BLENinebotMessage(com: UInt8(16), dat: [UInt8(22)])
-            if let cli = self.client{
-                 if let dat = message?.toNSData(){
-                    
-                    cli.writeValue(dat)
-                }
-            }
-            else{
-                timer.invalidate()
-                self.sendTimer = nil
-                self.appendToLog("Error : Intentant enviar missatge amb client = nil")
-            }
-        }
-    }
-    
+      
     // MARK: Other functions
     
     func appendToLog(s : String){
@@ -297,145 +312,28 @@ class ViewController: UIViewController {
         //self.tview.text = self.tview.text + "\n" + s
     }
     
-    func forwardWrite(data : NSData){
-        
-        if self.state == .MiM{
-            self.client!.writeValue(data)
-        }
-        
-        let msg = BLENinebotMessage(data: data)
-        
-        if let m = msg {
-            self.appendToLog(String(format: "<<< %d %d",m.command, m.data[0]))
-        }
-        
-    }
     
-    func forwardValue(data : NSData){
-        
-        if self.state == .MiM {
-            self.server!.updateValue(data)
-        }
-        
-        self.appendToBuffer(data)
-        self.procesaBuffer()
-        
-    }
     
-    //MARK: Connection management
-    
-    func appendToBuffer(data : NSData){
-        
-        
-        let count = data.length
-        var buf = [UInt8](count: count, repeatedValue: 0)
-        data.getBytes(&buf, length:count * sizeof(UInt8))
-        
-        buffer.appendContentsOf(buf)
-        
-        
-    }
-    
-    func procesaBuffer()
-    {
-        // Wait till header
-        
-        repeat {
-            
-            while buffer.count > 0 && buffer[0] != 0x55 {
-                buffer.removeFirst()
-            }
-            
-            // OK, ara hem de veure si el caracter següent es un aa
-            
-            if buffer.count < 2{    // Wait for more data
-                return
-            }
-            
-            if buffer[1] != 0xaa {  // Fals header. continue cleaning
-                buffer.removeFirst()
-                procesaBuffer()
-                return
-            }
-            
-            if buffer.count < 8 {   // Too small. Wait
-                return
-            }
-            
-            // Extract len and check size
-            
-            let l = Int(buffer[2])
-            
-            if l + 4 > 250 {
-                buffer.removeFirst(3)
-                return
-            }
-        
-            if buffer.count < (6 + l){
-                return
-            }
-            
-            // OK ara ja podem extreure el block. Te len + 6 bytes
-            
-            let block = Array(buffer[0..<(l+6)])
-            
-            
-            let msg = BLENinebotMessage(buffer: block)
-            
-            if let m = msg {
-                
-                let d = m.interpret()
-                
-                var updated = false
-                
-                for (k, v) in d {
-                    let w = datos.data[k].value
-                    if w != v{
-                        
-                        let name = BLENinebot.labels[k]
-                        self.appendToLog(String(format:"%@[%d] - %d", name, k, v))
-                        updated = true
-                    }
-        
-                    self.datos.addValue(k, value: v)
-                }
-                
-                if updated{
-                    if let dash = self.dashboard {
-                        dash.update()
-                    }
-        
-                }
-        
-               // self.appendToLog(String(format:">>> %@", m.interpret()))
-            }
-            
-            
-            buffer.removeFirst(l+6)
-            
-        } while buffer.count > 6
-        
-        
-    }
-    
-    func toScreen(){
-        self.appendToLog("Situacio actual de les variables ")
-        self.appendToLog("-------------------------------------------------")
-        for var k = 0; k < 256; k++ {
-            let v = datos.data[k].value
-            self.appendToLog(String(format:"%@[%d] : %d", BLENinebot.labels[k],  k, v))
-        }
-    }
     
     //MARK: View management
     
     override func viewWillAppear(animated: Bool) {
-        if self.state == .Client{
-            self.clientStopped()
-            
+        
+        if let dash = self.dashboard{
+            if dash.client != nil{
+                dash.stop(self)
+            }
+            self.dashboard = nil // Released dashboard
         }
-        else{
-            NSLog("Connexio misteriosa")
+        // Now reload all data foir
+        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+        
+        let docsUrl = appDelegate?.applicationDocumentsDirectory()
+        
+        if let docs = docsUrl{
+            
+            loadLocalDirectoryData(docs)
+            self.tableView.reloadData()
         }
     }
     
