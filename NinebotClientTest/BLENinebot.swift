a4f21d4c9fedab325c125c3f0f4f0b0c22957dbc
Paco Gorina <fgorina@gmail.com>
2016-02-24 17:41:56 +0100

    Afegida Llicència GPL 3.0

--- NinebotClientTest/BLENinebot.swift
+++ NinebotClientTest/BLENinebot.swift
@@ -5,6 +5,18 @@
 //  Created by Francisco Gorina Vanrell on 2/2/16.
 //  Copyright © 2016 Paco Gorina. All rights reserved.
 //
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//( at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // BLENinebot represents the state of the wheel
 //
@@ -592,10 +604,18 @@ class  BLENinebot : NSObject{
     }
     
     func speed(i : Int) -> Double {
-        let s : Double = Double(data[BLENinebot.kCurrentSpeed].log[i].value) / 1000.0
         
-        return s
-    }
+        if i < 2  {
+            return Double(data[BLENinebot.kCurrentSpeed].log[i].value) / 1000.0
+        }else{
+            let v0 = Double(data[BLENinebot.kCurrentSpeed].log[i - 2].value) / 1000.0
+            let v1 = Double(data[BLENinebot.kCurrentSpeed].log[i - 1].value) / 1000.0
+            let v2 = Double(data[BLENinebot.kCurrentSpeed].log[i].value) / 1000.0
+            
+            return (v0 + 2 * v1 + v2 )/4.0
+        }
+        
+      }
     
     
     // Speed limit
